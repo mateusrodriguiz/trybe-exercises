e0379a8ea2b61275564f3474164c28388b154ecb
Mateus Rodrigues <mateusrodriguiz@gmail.com>
2021-02-24 19:16:48 -0300

    Exercicio 01 finalizado

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
@@ -0,0 +1,22 @@
+/* EXERCICIO 1 */
+let a = 10;
+let b = 2;
+
+/* adição */
+let adicao = a + b;
+    console.log(adicao);
+
+
+/* subtração */
+let subtracao = a - b;
+    console.log(subtracao);
+
+/* multiplicação */
+let multiplicacao = a * b;
+    console.log(multiplicacao);
+
+/* divisao */
+let divisao = a / b;
+    console.log(divisao);
+
+/* modulo */
