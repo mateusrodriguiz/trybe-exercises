9537eadaf10dc1de553059e0fd5655a8f61b7b36
Mateus Rodrigues <mateusrodriguiz@gmail.com>
2021-02-25 16:23:28 -0300

    Exercicio 03 finalizado

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
@@ -1,4 +1,10 @@
-/* EXERCICIO 1 */
+/* EXERCICIO 1
+Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+Adição
+Subtração
+Multiplicação
+Divisão
+Módulo*/
 let a = 10;
 let b = 2;
 
@@ -19,4 +25,35 @@ let multiplicacao = a * b;
 let divisao = a / b;
     console.log(divisao);
 
-/* modulo */
+/* EXERCICIO 2
+Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados. */
+let a = 10;
+let b = 2;
+
+if (a > b) {
+    console.log(a);
+}
+else {
+    console.log(b);
+}
+
+
+/* EXERCICIO 3
+Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados. */
+let a = 100; b = 2; c = 20;
+
+if (a > (b && c)) {
+    console.log(a);
+}
+
+else if (b > (a && c)) {
+    console.log(b);
+}
+
+else if (c > (b && a)) {
+    console.log(c);
+}
+
+
+/* EXERCICIO 4
+Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário. */
