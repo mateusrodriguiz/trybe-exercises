10b4fac89ee7e5883283d537647b796d82607bf7
Mateus Rodrigues <mateusrodriguiz@gmail.com>
2021-02-26 16:14:04 -0300

    Finaizado exercicio 09

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_9.js
@@ -0,0 +1,8 @@
+/* Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 . */
+
+let score = [];
+
+for (index = 0; index <= 25; index += 1) {
+    score.push(index);
+    console.log(score[index] / 2);
+}
