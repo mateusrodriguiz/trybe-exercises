b09aa8af48344f1cd82838d6f1187051b07a7638
Mateus Rodrigues <mateusrodriguiz@gmail.com>
2021-03-01 12:34:07 -0300

    Finalizado exercicio 02

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise_2.js
@@ -0,0 +1,18 @@
+/* 
+2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+n = 5
+
+*
+**
+***
+****
+*****        */
+
+let number = '';
+
+for (index = 0; index < 5; index += 1) {
+    number += '*';
+    console.log(number);
+}
+
